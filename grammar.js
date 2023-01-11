2a92a64826a7001be443bcb0010e07d814d108d1
YourFin <d.junkpobox+git@gmail.com>
2023-01-11 13:26:08 -0800

    Pre-dump whitespace checks

--- grammar.js
+++ grammar.js
@@ -13,8 +13,8 @@ module.exports = grammar({
     _term: $ => choice(
       $.let,
       $.dup,
-      //$.lam,
-      //$.ctr,
+      $.lam,
+      $.ctr, // constructor
       //$.op2,
       //$.app,
       //$.sup,
@@ -26,7 +26,7 @@ module.exports = grammar({
       //$.bng, // bang
       //$.ask_sugar_named,
       //$.ask_sugar_anon,
-      //$.var,
+      $.var, // var in ast
     ),
     let: $ => seq(
       'let',
@@ -39,6 +39,7 @@ module.exports = grammar({
       optional($._ws), // ?
       field('body', $._term),
     ),
+
     dup: $ => seq(
       'dup',
       $._ws,
@@ -52,6 +53,19 @@ module.exports = grammar({
       optional($._ws), // ?
       field('body', $._term),
     ),
+
+    // https://github.com/Kindelia/HVM/blob/68769b257924920b24f7ded43a39ded58d8cbdf3/src/language/syntax.rs#L236
+    lam: $ => seq(
+      /[@λ]/,
+      // Add optional whitespace here?
+      field('name', $.identifier),
+      $._ws,
+      field('body', $._term),
+    ),
+
+
+    ctr: $ => seq('/[A-Z]/', repeat($._identifier_char)),
+
     _num: $ => choice(
       $.U60_dec_literal,
       $.U60_hex_literal,
@@ -60,13 +74,18 @@ module.exports = grammar({
     U60_dec_literal: $ => /\d(_?\d)*/,
     U60_hex_literal: $ => seq(/0[xX]/, /[0-9a-fA-F](_?[0-9a-fA-F])/),
     F60_literal: $ => /\d(_?\d)*(\.\d)?(_?\d)*([eE][\+-]?\d(_?\d)*)?/,
+
+    // https://github.com/Kindelia/HVM/blob/68769b257924920b24f7ded43a39ded58d8cbdf3/src/language/syntax.rs#L373
+    var: $ => seq('/[a-z_$]/', repeat($._identifier_char)),
     comment: $ => seq('//', /.*/),
-    _ws: $ => repeat1(choice(/\s+/, $.comment)),
+    _ws: $ => /\s+/,
     // https://github.com/Kindelia/HVM/blob/4974a8ae7c83b775aaf627719e638d06dac4a4dc/src/language/parser.rs#L352
     _identifier_char: $ => choice(/\w/, '.', '$'),
     identifier: $ => repeat1($._identifier_char),
-
-
-  }
+  },
+  extras: $ => [
+    $._ws,
+    $.comment,
+  ],
 })
 
